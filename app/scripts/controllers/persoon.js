ae9ef19307d684cc8afa21e9c73f565cbba45635
Xander Arling <xander.arling@gmail.com>
2013-09-23 22:38:04 +0200

    Nieuwe persoon model toevoegd.

--- app/scripts/controllers/persoon.js
+++ app/scripts/controllers/persoon.js
@@ -4,5 +4,21 @@
 angular.module('frontendApp')
   .controller('PersoonCtrl', ['$scope', 'Persoon', function ($scope, Persoon) {
 
+    // init get all personen
     $scope.personen = Persoon.query();
+
+    $scope.saveNew = function() {
+
+      Persoon.save($scope.nieuwPersoon, function(n) {
+        // empty persoon at the end
+        $scope.nieuwPersoon = {};
+      }, function(n) {
+        // hier gaat het mis. Nu maar even alert
+        console.debug("sjonge, deze ging niet lekker" + n );
+        alert("probleem met het opslaan");
+      });
+
+
+    };
+
   }]);
