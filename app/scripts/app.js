9fc1318f26a266ab8ebd38833fad5b44b33d99db
Xander Arling <xander.arling@gmail.com>
2013-10-28 07:26:45 +0100

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,22 +7,6 @@ angular.module('frontendApp', ['nn.config', 'ngRoute', 'ui.bootstrap', 'ngResour
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/personen', {
-        templateUrl: 'views/personen.html',
-        controller: 'PersoonCtrl'
-      })
-      .when('/skills', {
-        templateUrl: 'views/skill.html',
-        controller: 'SkillCtrl'
-      })
-      .when('/persoon-toevoegen', {
-        templateUrl: 'views/persoon.html',
-        controller: 'PersoonCtrl'
-      })
-      .when('/persoon-aanpassen/:persoonId', {
-        templateUrl: 'views/persoon.html',
-        controller: 'EditPersoonCtrl'
-      })
       .otherwise({
         redirectTo: '/'
       });
